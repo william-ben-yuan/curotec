40666ad3a495df0ba3138cbd9f4f7a800ef5178d
Will <wllmdd@gmail.com>
2025-05-23 00:40:15 -0300

    feat: create post requests

---
+++ app/Http/Requests/Posts/PostUpdateRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'sometimes|required|string|max:255',
+            'content' => 'sometimes|required|string',
+            'status' => 'sometimes|required|in:draft,published,archived',
+            'published_at' => 'nullable|date|required_if:status,published',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'published_at.required_if' => 'The publication date is required when the status is "published".',
+        ];
+    }
+}
